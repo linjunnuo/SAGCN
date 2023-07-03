fbe5f00f6d492027148b4c2ce6c77941f1287366
linjunnuo <limchvnno@gmail.com>
2023-07-03 10:54:35 +0800

    update

--- EdgeiiotData/CNN.py
+++ EdgeiiotData/CNN.py
@@ -16,7 +16,7 @@ while True:
     i += 1
 TensorWriter = SummaryWriter(dir)
 
-# 通过键来访问加载的数据集
+# ------------------------------- 通过键来访问加载的数据集 ------------------------------- #
 X_train = loaded_data['X_train']
 X_test = loaded_data['X_test']
 y_train = loaded_data['y_train']
@@ -24,17 +24,17 @@ y_test = loaded_data['y_test']
 
 
 
-# 首先，我们需要将 numpy 数组转换为 PyTorch 张量
+# ---------------------- 首先，我们需要将 numpy 数组转换为 PyTorch 张量 --------------------- #
 X_train = torch.tensor(X_train, dtype=torch.float32)
 X_test = torch.tensor(X_test, dtype=torch.float32)
 y_train = torch.tensor(y_train, dtype=torch.float32)  # 可能需要在此步骤将独热编码标签转化为类别
 y_test = torch.tensor(y_test, dtype=torch.float32)
 
-# 然后，我们将张量包装为 TensorDataset 对象
+# ----------------------- 然后，我们将张量包装为 TensorDataset 对象 ----------------------- #
 train_dataset = TensorDataset(X_train, y_train)
 test_dataset = TensorDataset(X_test, y_test)
 
-# 最后，我们创建 DataLoader 对象
+# --------------------------- 最后，我们创建 DataLoader 对象 -------------------------- #
 batch_size = 64  # 你可以选择适合你的硬件和数据的批量大小
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
